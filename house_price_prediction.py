5f432499e40a6f77eb2e3e0e39239fa3a5a0859f
Alnise Bitoki <alnisebitoki@gmail.com>
2023-05-04 15:43:32 +0000

    first commit

---
+++ house_price_prediction.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import sklearn
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+# Pour ouvrir le model afin de pouvoir l'utiliser
+# 
+model = pickle.load(open('regression_model.sav', 'rb'))
+
+st.title('Prediction des prix de maison')
+st.sidebar.header('Données de la maison')
+image = Image.open('maison.jpg')
+st.image(image, '')
+
+# FUNCTION
+
+
+def house_report():
+  OverallQual = st.sidebar.slider('OverallQual', 1,10, 1)
+  ExterQual = st.sidebar.slider('ExterQual', 1,4, 1)
+  Neighborhood = st.sidebar.slider('Neighborhood', 1,24, 1)
+  GarageCars = st.sidebar.slider('GarageCars', 0,2, 0)
+  GrLivArea = st.sidebar.slider('GrLivArea', 5,9, 1)
+  YearBuilt = st.sidebar.slider('YearBuilt', 1900,2000, 1900) 
+  TotalBsmtSF = st.sidebar.slider('TotalBsmtSF', 0,9, 1)
+  BsmtQual = st.sidebar.slider('BsmtQual', 0,5, 1)
+  GarageArea = st.sidebar.slider('GarageArea', 0,8, 0)
+  KitchenQual = st.sidebar.slider('KitchenQual', 1,4, 1)
+  stFlrSF = st.sidebar.slider('1stFlrSF', 5,9, 1)
+  FullBath = st.sidebar.slider('FullBath', 0,2, 0)
+
+
+
+  house_report_data = {
+      'OverallQual':OverallQual,
+      'ExterQual':ExterQual,
+      'Neighborhood':Neighborhood,
+      'GarageCars':GarageCars,
+      'GrLivArea':GrLivArea,
+      'YearBuilt':YearBuilt,
+      'TotalBsmtSF':TotalBsmtSF,
+      'BsmtQual':BsmtQual,
+      'GarageArea':GarageArea,
+      'KitchenQual':KitchenQual,
+      'stFlrSF':stFlrSF,
+      'FullBath':FullBath
+  }
+  report_data = pd.DataFrame(house_report_data, index=[0])
+  return report_data
+
+house_data = house_report()
+st.header('Données de la maison')
+st.write(house_data)
+
+house_price = model.predict(house_data)
+st.subheader('Prix de la maison')
+st.subheader(str(np.round(house_price[0], 2)) + ' FCFA')
