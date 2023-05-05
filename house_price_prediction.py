305efef66ee9535c845677f4617383af2eeecd5d
Alnise Bitoki <alnisebitoki@gmail.com>
2023-05-05 15:00:02 +0000

    stable: formatage du prix et séparateur

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+import math
 from PIL import Image
 
 model = pickle.load(open('model/regression_model.sav', 'rb'))
@@ -12,20 +13,18 @@ image = Image.open('img/maison.jpg')
 st.image(image, '')
 
 def house_report():
-  OverallQual = st.sidebar.slider('OverallQual', 1,10, 1)
-  ExterQual = st.sidebar.slider('ExterQual', 1,4, 1)
-  Neighborhood = st.sidebar.slider('Neighborhood', 1,24, 1)
-  GarageCars = st.sidebar.slider('GarageCars', 0,2, 0)
-  GrLivArea = st.sidebar.slider('GrLivArea', 5,9, 1)
-  YearBuilt = st.sidebar.slider('YearBuilt', 1900,2000, 1900) 
-  TotalBsmtSF = st.sidebar.slider('TotalBsmtSF', 0,9, 1)
-  BsmtQual = st.sidebar.slider('BsmtQual', 0,5, 1)
-  GarageArea = st.sidebar.slider('GarageArea', 0,8, 0)
-  KitchenQual = st.sidebar.slider('KitchenQual', 1,4, 1)
-  stFlrSF = st.sidebar.slider('1stFlrSF', 5,9, 1)
-  FullBath = st.sidebar.slider('FullBath', 0,2, 0)
-
-
+  OverallQual = st.sidebar.slider('(OverallQual)', 1,10, 1)
+  ExterQual = st.sidebar.slider('(ExterQual)', 1,4, 1)
+  Neighborhood = st.sidebar.slider('(Neighborhood)', 1,24, 1)
+  GarageCars = st.sidebar.slider('(GarageCars)', 0,2, 0)
+  GrLivArea = st.sidebar.slider('(GrLivArea)', 5,9, 1)
+  YearBuilt = st.sidebar.slider('(YearBuilt)', 1900,2000, 1900) 
+  TotalBsmtSF = st.sidebar.slider('(TotalBsmtSF)', 0,9, 1)
+  BsmtQual = st.sidebar.slider('(BsmtQual)', 0,5, 1)
+  GarageArea = st.sidebar.slider('(GarageArea)', 0,8, 0)
+  KitchenQual = st.sidebar.slider('(KitchenQual)', 1,4, 1)
+  stFlrSF = st.sidebar.slider('(1stFlrSF)', 5,9, 1)
+  FullBath = st.sidebar.slider('(FullBath)', 0,2, 0)
 
   house_report_data = {
       'OverallQual':OverallQual,
@@ -44,10 +43,30 @@ def house_report():
   report_data = pd.DataFrame(house_report_data, index=[0])
   return report_data
 
+def format_price(number, spacing = ' '):
+  number_reversed = str(number)[::-1]
+  ret_str = ''
+  count = 1
+
+  if len(number_reversed) > 3:
+    ret = ''
+    for c in number_reversed:
+      ret = ret + c + (spacing if count == 3 else '')
+
+      if count == 3: count = 0
+      count = count + 1
+
+    ret_str = ret[::-1]
+  else:
+    ret_str = str(number)
+
+  return ret_str[1:] if ret_str[0] == ' ' else ret_str
+
 house_data = house_report()
 st.header('Données de la maison')
 st.write(house_data)
 
 house_price = model.predict(house_data)
-st.subheader('Prix = :red[{} FCFA]'.format(str(np.round(house_price[0], 2))))
-st.markdown('Fait **_par_ Alnise BITOKI**.')
+st.subheader('Prix = :red[{}] FCFA'.format(format_price(math.floor(np.round(house_price[0], 2)))))
+st.divider()
+st.markdown('Fait  _par_  **Alnise BITOKI**.')
